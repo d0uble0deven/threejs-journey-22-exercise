26a0edd5bf6257e53f491c313863a74ad6cfc7dd
Dev Govindji <dev.govindji.software@gmail.com>
2024-01-29 18:02:10 -0600

    final commit

--- src/script.js
+++ src/script.js
@@ -2,6 +2,8 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
 
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+
 /**
  * Base
  */
@@ -13,6 +15,18 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+
+/**
+ * Lights
+ */
+// Ambient light
+const ambientLight = new THREE.AmbientLight('#ffffff', 0.9)
+scene.add(ambientLight)
+
+// Directional Light
+const directionalLight = new THREE.DirectionalLight('#ffffff', 2.1)
+directionalLight.position.set(1, 2, 3)
+scene.add(directionalLight)
 
 /**
  * Objects
@@ -35,6 +49,28 @@ const object3 = new THREE.Mesh(
 object3.position.x = 2
 
 scene.add(object1, object2, object3)
+
+/**
+ * Raycster
+ */
+const raycaster = new THREE.Raycaster()
+
+// const rayOrigin = new THREE.Vector3(-3, 0, 0)
+// const rayDirection = new THREE.Vector3(10, 0, 0)
+// rayDirection.normalize()
+
+// object1.updateMatrixWorld()
+// object2.updateMatrixWorld()
+// object3.updateMatrixWorld()
+
+// raycaster.set(rayOrigin, rayDirection)
+
+// const intersect = raycaster.intersectObject(object2)
+// console.log(intersect)
+
+// const intersects = raycaster.intersectObjects([object1, object2, object3])
+// console.log(intersects)
+
 
 /**
  * Sizes
@@ -60,6 +96,37 @@ window.addEventListener('resize', () =>
 })
 
 /**
+ * Mouse
+*/
+const mouse = new THREE.Vector2();
+
+window.addEventListener('mousemove', (_event) => {
+    mouse.x = _event.clientX / sizes.width * 2 - 1
+    mouse.y = - (_event.clientY / sizes.width) * 2 + 1
+})
+
+window.addEventListener('click', () => {
+    if (currentIntersect) {
+
+        switch (currentIntersect.object){
+            case object1:
+                console.log('click on object 1')
+                break;
+
+            case object2:
+                console.log('click on object 2')
+                break;
+
+            case object3:
+                console.log('click on object 3')
+                break;
+        }
+
+    }
+})
+
+
+/**
  * Camera
  */
 // Base camera
@@ -81,14 +148,101 @@ renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**
+ * Model
+ */
+const gltfLoader = new GLTFLoader()
+
+let model = null
+
+gltfLoader.load('./models/Duck/glTF-Binary/Duck.glb', 
+    (gltf) =>{
+        model = gltf.scene
+        model.position.y = - 1.2
+        scene.add(model)
+    }
+)
+
+
+
+/**
  * Animate
  */
 const clock = new THREE.Clock()
+
+let currentIntersect = null
 
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
 
+    // Animate objects
+    object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5
+    object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5
+    object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5
+
+    // Cast a ray
+    raycaster.setFromCamera(mouse, camera)
+
+    const objectToTest = [object1, object2, object3]
+
+    const intersects = raycaster.intersectObjects(objectToTest)
+
+    for (const object of objectToTest){
+        object.material.color.set('#ff0000')
+    }
+
+    for (const intersect of intersects){
+        intersect.object.material.color.set('#0000ff')
+    }
+
+    if(intersects.length) {
+        if (currentIntersect === null) {
+            console.log('mouse enter')   
+        }
+        currentIntersect = intersects[0]
+    } else {
+        if (currentIntersect === null) {
+            console.log('mouse leave')
+        }
+        currentIntersect = null
+    }
+
+    if (model) {
+        const modelIntersects = raycaster.intersectObject(model)
+        console.log(modelIntersects)
+
+        if (modelIntersects.length) {
+            model.scale.set(1.2, 1.2, 1.2)
+        } else {
+            model.scale.set(1, 1, 1)
+        }
+
+    }
+
+    // const rayOrigin = new THREE.Vector3(-3, 0, 0)
+    // const rayDirection = new THREE.Vector3(1, 0, 0)
+    // rayDirection.normalize()
+
+    // object1.updateMatrixWorld()
+    // object2.updateMatrixWorld()
+    // object3.updateMatrixWorld()
+
+    // raycaster.set(rayOrigin, rayDirection)
+    
+    // // Shoot the ray
+    // const objectToTest = [object1, object2, object3]
+
+    // const intersects = raycaster.intersectObjects(objectToTest)
+    // console.log(intersects.length)
+
+    // for (const object of objectToTest){
+    //     object.material.color.set('#ff0000')
+    // }
+
+    // for (const intersect of intersects){
+    //     intersect.object.material.color.set('#0000ff')
+    // }
+    
     // Update controls
     controls.update()
 
